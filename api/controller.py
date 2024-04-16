39e500bb692bdb05302166787bf2f4158be7067e
Admin <nguyenthanhlong19092002@gmail.com>
2024-04-16 17:21:49 +0700

    commit v4

--- api/controller.py
+++ api/controller.py
@@ -1,24 +1,21 @@
 from flask import request
 from .service import validateInfo, reportPdf
 
-def controller(app):
-    @app.route("/hello")
-    def sayHi():
-        return "Hello world"
+def sayHi():
+    return "Hello world"
 
-    @app.route("/expert_01", methods=['POST'])
-    def expert01():
-        messageArr = []
-        validateInfo('institution', request.json, messageArr)
-        validateInfo('date', request.json, messageArr)
-        validateInfo('name', request.json, messageArr)
-        validateInfo('age', request.json, messageArr)
-        validateInfo('gender', request.json, messageArr)
+def report01():
+    messageArr = []
+    validateInfo('institution', request.json, messageArr)
+    validateInfo('date', request.json, messageArr)
+    validateInfo('name', request.json, messageArr)
+    validateInfo('age', request.json, messageArr)
+    validateInfo('gender', request.json, messageArr)
         
-        if(len(messageArr) > 0):
-            return {
-                'status': False,
-                'messages':messageArr
-            }
-        data = request.json
-        return reportPdf('expert_01.html', data)
+    if(len(messageArr) > 0):
+        return {
+            'status': False,
+            'messages':messageArr
+        }
+    data = request.json
+    return reportPdf('expert_01.html', data)
