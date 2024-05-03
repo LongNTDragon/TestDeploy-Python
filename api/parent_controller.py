d7059b3ffbf8a10d9a22522916e9bae9669f4dd5
sSyRuSs <89365131+sSyRuSs@users.noreply.github.com>
2024-05-04 01:06:02 +0700

    feature: Add Parent Controller

---
+++ api/parent_controller.py
@@ -0,0 +1,152 @@
+from flask import request
+from .service import validateInfo, reportPdf, drawHexagon, drawScatterPlot, drawHeartRatePlot
+
+def parent01():
+    messageArr = []
+    validateInfo('institution', request.json, messageArr)
+    validateInfo('date', request.json, messageArr)
+    validateInfo('name', request.json, messageArr)
+    validateInfo('age', request.json, messageArr)
+    
+    if(len(messageArr) > 0):
+        return {
+            'status': False,
+            'messages':messageArr
+        }
+    data = request.json
+    return reportPdf('parent_01.html', data)
+
+# def parent02():
+#     messageArr = []
+#     validateInfo('date', request.json, messageArr)
+#     validateInfo('name', request.json, messageArr)
+#     validateInfo('age', request.json, messageArr)
+    
+#     if(len(messageArr) > 0):
+#         return {
+#             'status': False,
+#             'messages':messageArr
+#         }
+    
+#     data = request.json
+#     drawHexagon(data)
+#     return reportPdf('parent_02.html', data)
+
+def parent03():
+    messageArr = []
+    validateInfo('factor1', request.json, messageArr)
+    validateInfo('factor2', request.json, messageArr)
+    validateInfo('factor3', request.json, messageArr)
+    validateInfo('factor4', request.json, messageArr)
+    validateInfo('factor5', request.json, messageArr)
+    validateInfo('factor6', request.json, messageArr)
+    
+    validateInfo('date', request.json, messageArr)
+    validateInfo('name', request.json, messageArr)
+    validateInfo('age', request.json, messageArr)
+    
+    if(len(messageArr) > 0):
+        return {
+            'status': False,
+            'messages':messageArr
+        }
+    
+    data = request.json
+    drawScatterPlot(data)
+    return reportPdf('parent_03.html', data)
+
+def parent04():
+    messageArr = []
+    validateInfo('date', request.json, messageArr)
+    validateInfo('name', request.json, messageArr)
+    validateInfo('age', request.json, messageArr)
+    
+    if(len(messageArr) > 0):
+        return {
+            'status': False,
+            'messages':messageArr
+        }
+    
+    data = request.json
+    drawHeartRatePlot(data)
+    return reportPdf('parent_04.html', data)
+
+def parent05():
+    messageArr = []
+    validateInfo('date', request.json, messageArr)
+    validateInfo('name', request.json, messageArr)
+    validateInfo('age', request.json, messageArr)
+    
+    if(len(messageArr) > 0):
+        return {
+            'status': False,
+            'messages':messageArr
+        }
+    
+    data = request.json
+    drawScatterPlot(data)
+    return reportPdf('parent_05.html', data)
+
+def parent06():
+    messageArr = []
+    validateInfo('date', request.json, messageArr)
+    validateInfo('name', request.json, messageArr)
+    validateInfo('age', request.json, messageArr)
+    
+    if(len(messageArr) > 0):
+        return {
+            'status': False,
+            'messages':messageArr
+        }
+    
+    data = request.json
+    drawHeartRatePlot(data)
+    return reportPdf('parent_06.html', data)
+
+def parent07():
+    messageArr = []
+    validateInfo('date', request.json, messageArr)
+    validateInfo('name', request.json, messageArr)
+    validateInfo('age', request.json, messageArr)
+    
+    if(len(messageArr) > 0):
+        return {
+            'status': False,
+            'messages':messageArr
+        }
+    
+    data = request.json
+    drawScatterPlot(data)
+    return reportPdf('parent_07.html', data)
+
+def parent08():
+    messageArr = []
+    validateInfo('date', request.json, messageArr)
+    validateInfo('name', request.json, messageArr)
+    validateInfo('age', request.json, messageArr)
+    
+    if(len(messageArr) > 0):
+        return {
+            'status': False,
+            'messages':messageArr
+        }
+    
+    data = request.json
+    drawHeartRatePlot(data)
+    return reportPdf('parent_08.html', data)
+
+def parent09():
+    messageArr = []
+    validateInfo('date', request.json, messageArr)
+    validateInfo('name', request.json, messageArr)
+    validateInfo('age', request.json, messageArr)
+    
+    if(len(messageArr) > 0):
+        return {
+            'status': False,
+            'messages':messageArr
+        }
+    
+    data = request.json
+    drawScatterPlot(data)
+    return reportPdf('parent_09.html', data)
